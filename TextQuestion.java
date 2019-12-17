28c51ef66eb4bc8c53fad3504fd644f306aeec30
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-17 10:24:43 -0500

    Add files via upload

---
+++ TextQuestion.java
@@ -0,0 +1,36 @@
+public class TextQuestion
+{
+	private String type;
+	private String question;
+	private String answer;
+	
+	public TextQuestion(String questionType, String theQuestion, String questionAnswer)
+	{
+		type = questionType;
+		question = theQuestion;
+		answer = questionAnswer;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public String getQuestion()
+	{
+		return question;
+	}
+	
+	public String getAnswer()
+	{
+		return answer;
+	}
+	
+	public int compareTo(TextQuestion response)
+	{
+		if(type.equals(response.getAnswer()))
+			return 1;
+		else
+			return -1;
+	}
+}
