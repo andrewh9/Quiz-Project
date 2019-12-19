c3d118ba682ee37fc9e6013c19462dac8343b035
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-19 10:18:29 -0500

    Update NQuestion.java

--- NQuestion.java
+++ NQuestion.java
@@ -1,4 +1,28 @@
-public class NQuestion extends Question
+public class NumberQuestion extends Question
 {
+    private String type;
+    private String question;
+    private String answer;
 
+    public NumberQuestion(String questionType, String theQuestion, String questionAnswer)
+    {
+      type = questionType;
+      question = theQuestion;
+      answer = questionAnswer;
+    }
+    
+    public String getType()
+    {
+      return type;
+    }
+    
+    public String getQuestion()
+    {
+      return question;
+    }
+    
+    public String getAnswer()
+    {
+      return answer;
+    }
 }
