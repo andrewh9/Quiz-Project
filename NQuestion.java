fb7b3c7f8e2025e9085c2c5f41398e649e480480
bnathabheem <58562077+bnathabheem@users.noreply.github.com>
2019-12-19 09:59:51 -0500

    Update NQuestion.java

--- NQuestion.java
+++ NQuestion.java
@@ -1,13 +1,4 @@
-public class NQuestion implements Question
+public class NQuestion extends Question
 {
 
-    @Override
-    public int compareTo() {
-        return 0;
-    }
-
-    @Override
-    public int compareTo(Question o) {
-        return 0;
-    }
 }
