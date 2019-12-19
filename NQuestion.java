05376dbc11cbfeb379436fcb46fb8082646fe1b2
bnathabheem <58562077+bnathabheem@users.noreply.github.com>
2019-12-18 22:37:17 -0500

    Create NQuestion.java

---
+++ NQuestion.java
@@ -0,0 +1,13 @@
+public class NQuestion implements Question
+{
+
+    @Override
+    public int compareTo() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Question o) {
+        return 0;
+    }
+}
