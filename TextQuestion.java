a0afb056dbf9622d759f8868c5629ed2c5960fbc
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-28 21:17:58 -0500

    Update TextQuestion.java

--- TextQuestion.java
+++ TextQuestion.java
@@ -4,27 +4,10 @@ public class TextQuestion extends Question
 	private String question;
 	private String answer;
 	
-	public TextQuestion(String questionType, String theQuestion, String questionAnswer)
+	public Question(String questionType, String theQuestion, String questionAnswer)
 	{
 		type = questionType;
 		question = theQuestion;
 		answer = questionAnswer;
-	}
-	
-	public String getType()
-	{
-		return type;
-	}
-	
-	public String getQuestion()
-	{
-		return question;
-	}
-	
-	public String getAnswer()
-	{
-		return answer;
-	}
-	
-	
+	}	
 }
