a2ba67260de8416339c2cbc186997582c096ebe8
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-28 21:19:40 -0500

    Update and rename NQuestion.java to NumberQuestion.java

---
+++ NumberQuestion.java
@@ -0,0 +1,14 @@
+public class NumberQuestion extends Question
+{
+    private String type;
+    private String question;
+    private String answer;
+
+    public Question(String questionType, String theQuestion, String questionAnswer)
+    {
+      type = questionType;
+      question = theQuestion;
+      answer = questionAnswer;
+    }
+    
+}
