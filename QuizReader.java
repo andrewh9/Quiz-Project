4b4d93de44a23797a8cd70aad6a1470159e39844
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-28 22:32:37 -0500

    Update QuizReader.java

--- QuizReader.java
+++ QuizReader.java
@@ -1,3 +1,18 @@
-public class QuizReader extends QuizGrader
+import java.util.Scanner;
+
+public class Quiz
 {
+		public abstract void main(String[] args)
+		{
+   		FileReader file = new FileReader("quiz.txt");
+			Scanner s = new Scanner (file);
+		
+			String type = s.next();
+	
+			if(type.equals("T"))
+			{
+					String question = s.nextLine();
+					String answer = s.nextLine();
+			}
+		}
 }
