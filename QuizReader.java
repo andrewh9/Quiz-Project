2ab2d21c59c83d4fdcbc6bba092945536526d82f
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2019-12-28 22:33:18 -0500

    Update QuizReader.java

--- QuizReader.java
+++ QuizReader.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Quiz
 {
-		public abstract void main(String[] args)
-		{
+	public abstract void main(String[] args)
+	{
    		FileReader file = new FileReader("quiz.txt");
-			Scanner s = new Scanner (file);
+		Scanner s = new Scanner (file);
 		
-			String type = s.next();
+		String type = s.next();
 	
-			if(type.equals("T"))
-			{
-					String question = s.nextLine();
-					String answer = s.nextLine();
-			}
+		if(type.equals("T"))
+		{
+			String question = s.nextLine();
+			String answer = s.nextLine();
 		}
+	}
 }
