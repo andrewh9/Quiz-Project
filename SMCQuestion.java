849124dba1321c3d397c2019d47ab8ff08466c8f
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-01 23:17:28 -0500

    Update SMCQuestion.java

--- SMCQuestion.java
+++ SMCQuestion.java
@@ -3,12 +3,14 @@ public class SMCQuestion extends Question
     private String type;
     private String question;
     private String answer;
+    private String[] choices;
 
-    public SMCQuestion(String questionType, String theQuestion, String questionAnswers)
+    public SMCQuestion(String questionType, String theQuestion, String questionAnswers, String[] answerChoices)
     {
         type = questionType;
         question = theQuestion;
         answers = questionAnswer;
+        choices = answerChoices;
     }
 
     public String getType()
