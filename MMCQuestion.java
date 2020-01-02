bd008f640b599edd88a6113d4c3cfb3fae3e6e47
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-02 01:00:06 -0500

    Update MMCQuestion.java

--- MMCQuestion.java
+++ MMCQuestion.java
@@ -3,14 +3,12 @@ public class MMCQuestion extends Question
     private String type;
     private String question;
     private String answer;
-    private String[] choices;
 
-    public MMCQuestion(String questionType, String theQuestion, String questionAnswer, String[] answerChoices)
+    public MMCQuestion(String questionType, String theQuestion, String questionAnswer)
     {
       type = questionType;
       question = theQuestion;
       answer = questionAnswer;
-      choices = answerChoices;
     }
     
     public String getType()
