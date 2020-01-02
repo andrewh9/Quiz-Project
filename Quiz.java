7f8828e83fd3ece9aafb741726b471aa6afba922
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-02 01:20:39 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,4 +1,30 @@
-public class Quiz
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Quiz extends QuizReader
 {
-   
+	String[] responses = new String[4];
+
+	public abstract void main(String[] args)
+	{
+		TextQuestion text = new TextQuestion("T", questions[0], answers[0]);
+		SMCQuestion SMC = new SMCQuestion("S", questions[1], answers[1], choicesSMC);
+		MMCQuestion MMC = new MMCQuestion("M", questions[2], answers[2], choicesMMC);
+		NumberQuestion number = new NumberQuestion("N", questions[3], answers[3]);
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println(text.getQuestion());
+		responses[0] = s.next();
+		
+		System.out.println(SMC.getQuestion());
+		responses[1] = s.next();
+		
+		System.out.println(MMC.getQuestion() + " Enter answers in listed order with only spaces separating them.");
+		responses[2] = s.nextLine();
+		
+		System.out.println(number.getQuestion());
+		Double numberResponse = s.nextDouble();
+		responses[3] = numberResponse.toString();
+	}
 }
