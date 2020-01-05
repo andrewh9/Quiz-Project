6a1dc204ca3c0de04cd38a5ea459e4bdf553a45f
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-05 13:01:01 -0500

    Update QuizReader.java

--- QuizReader.java
+++ QuizReader.java
@@ -10,67 +10,71 @@ public class Quiz
 	ArrayList<String> choicesSMC = new ArrayList<String>();
 	ArrayList<String> choicesMMC = new ArrayList<String>();
 	
-	FileReader file = new FileReader("quiz.txt");
-	Scanner s = new Scanner (file);
-		
-	for(int i = 0; i < 4; i++)
+	public abstract void main(String[] args) throws FileNotFoundException
 	{
-		String type = s.next();
+		FileReader file = new FileReader("quiz.txt");
+		Scanner s = new Scanner (file);
+		
+		for(int i = 0; i < 4; i++)
+		{
+			String type = s.next();
 	
-		if(type.equals("T"))
-		{
-			questions[0] = s.nextLine();
-			answers[0] = s.nextLine();
-		}
+			if(type.equals("T"))
+			{
+				questions[0] = s.nextLine();
+				answers[0] = s.nextLine();
+			}
 			
-		else if(type.equals("S"))
-		{
-			questions[1] = s.nextLine();
-				
-			for(int j = 0; j < 100; j++)
+			else if(type.equals("S"))
 			{
-				String correctness = s.next();
+				questions[1] = s.nextLine();
 				
-				String choice = s.next();
+				for(int j = 0; j < 100; j++)
+				{
+					String correctness = s.next();
 				
-				if(correctness.equals("+")
-				  	answers[1] = choice;
-				if(correctness.equals("+") || correctness.equals("-"))
-					choicesSMC.add(choice);
-				else if(! correctness.equals("-") && ! correctness.equals("+"))
-					break;
+					String choice = s.next();
+				
+					if(correctness.equals("+")
+				  		answers[1] = choice;
+					if(correctness.equals("+") || correctness.equals("-"))
+						choicesSMC.add(choice);
+					else if(! correctness.equals("-") && ! correctness.equals("+"))
+					    break;
+				}
 			}
-		}
 					   
-		else if(type.equals("M")
-		{
-			questions[2] = s.nextLine();
-				
-			answers[2] = "";
-				
-			for(int k = 0; k < 100; k++)
+			else if(type.equals("M")
 			{
-				String correctness1 = s.next();
+				questions[2] = s.nextLine();
+				
+				answers[2] = "";
+				
+				for(int k = 0; k < 100; k++)
+				{
+					String correctness1 = s.next();
 						
-				String choice1 = s.next();
+					String choice1 = s.next();
 					
-				if(correctness1.equals("+")
-					  answers[2] += choice1 + " ";
-				if(correctness1.equals("+") || correctness1.equals("-"))
-					choicesMMC.add(choice1);
-				else if(! correctness1.equals("-") && ! correctness1.equals("+"))
-					break;
+					if(correctness1.equals("+")
+					   answers[2] += choice1 + " ";
+					if(correctness1.equals("+") || correctness1.equals("-"))
+						choicesMMC.add(choice1);
+					else if(! correctness1.equals("-") && ! correctness1.equals("+"))
+					   break;
+				}
+				
 			}
-				
-		}
 					   
-		else if(type.equals("N")
-		{
-			questions[3] = s.nextLine();
+			else if(type.equals("N")
+			{
+				questions[3] = s.nextLine();
 				
-			Double number = (int)(s.nextDouble() * 10 + 0.5) / 10.0;
+				Double number = (int)(s.nextDouble() * 10 + 0.5) / 10.0;
 				
-			answers[3] = number.toString();
+				answers[3] = number.toString();
+			}
 		}
+		
 	}
 }
