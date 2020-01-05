099e451309e01650d753b3c1c6269a384dc3dd43
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-05 13:33:26 -0500

    Update SMCQuestion.java

--- SMCQuestion.java
+++ SMCQuestion.java
@@ -26,11 +26,11 @@ public class SMCQuestion extends Question
        return answer;
     }
     
-    public boolean gradeResponse(String response)
+    public String gradeResponse(String response)
     {
         if(response.equals(answer))
-            return true;
+            return "Correct";
         else
-            return false;
+            return "Incorrect";
     }
 }
