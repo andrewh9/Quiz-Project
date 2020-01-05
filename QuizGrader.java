120532e458deda1718cd5431397bd7f414e96ae6
andrewh9 <58561758+andrewh9@users.noreply.github.com>
2020-01-05 13:38:04 -0500

    Update QuizGrader.java

--- QuizGrader.java
+++ QuizGrader.java
@@ -1,4 +1,12 @@
 public class QuizGrader extends Quiz
 {
-	System.out.println("Text Question: " + 
+	System.out.println("Your Quiz Results");
+	
+	System.out.println("Text Question: " + text.gradeResponse(responses[0]));
+	
+	System.out.println("Single Choice Question: " + SMC.gradeResponse(responses[1]));
+	
+	System.out.println("Multiple Choice Question: " + MMC.gradeResponse(responses[2]));
+	
+	System.out.println("Number Question: " + number.gradeResponse(responses[3]));
 }
